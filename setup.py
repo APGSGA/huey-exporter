07ca02564fe476d4758729497496a200ce11d2a7
Severin Bühler <severin.buehler@apgsga.ch>
2019-08-02 09:29:45 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
         name='huey-exporter',
-        version='0.1.1',
-        description=' Huey exporter for Prometheus',
-        url='https://github.com/MarcoGlauser/huey-exporter',
-        author='Marco Glauser',
+        version='1.0.0.dev1',
+        description=' Hueyx exporter for Prometheus',
+        url='https://github.com/apgsga/huey-exporter',
+        author='Severin Bühler',
         license='MIT',
         packages=find_packages(),
         include_package_data=True,
         install_requires=[
             'prometheus_client>=0.2.0',
-            'huey>=1.9.0',
             'click>=6.7',
             'redis>=2.10.6',
         ],
