1002236dbb461c28684ecbb0a9c0b6aadbd7af6e
Severin Bühler <severin.buehler@apgsga.ch>
2019-08-07 08:51:21 +0200

    log error in thread

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
         name='huey-exporter',
-        version='1.0.1.dev1',
+        version='1.0.1.dev2',
         description=' Hueyx exporter for Prometheus',
         url='https://github.com/apgsga/huey-exporter',
         author='Severin Bühler',
